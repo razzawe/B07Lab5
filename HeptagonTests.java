b664c06dff92f58acd4bf27acf7952be2910ff41
AntSwift1 <antswift10@gmail.com>
2024-06-25 19:45:28 -0400

    fix: cleaned up indentations

--- HeptagonTests.java
+++ HeptagonTests.java
@@ -5,43 +5,43 @@ import org.junit.jupiter.api.Test;
 class HeptagonTests {
 	@Test
     void testGetArea() {
-          Heptagon h = new Heptagon(1, 12, new Point(1, 1));
-          assertEquals(h.getArea(), (7/4) * (1/Math.tan(Math.pi/7)), 0.01);
+        Heptagon h = new Heptagon(1, 12, new Point(1, 1));
+        assertEquals(h.getArea(), (7/4) * (1/Math.tan(Math.pi/7)), 0.01);
     }
 	
 	@Test
     void testGetAreaZero() {
-          Heptagon h = new Heptagon(0, 12, new Point(1, 1));
-          assertEquals(h.getArea(), 0);
+        Heptagon h = new Heptagon(0, 12, new Point(1, 1));
+        assertEquals(h.getArea(), 0);
     }
     
     @Test
     void testGetPerimeterZero() {
         Heptagon h = new Heptagon(0, -50, new Point(-12, 0));
-          assertEquals(h.getPerimeter(), 0);
+        assertEquals(h.getPerimeter(), 0);
     }
     
     @Test
     void testGetRadiusCornerZero() {
         Heptagon h = new Heptagon(0, 21, new Point(-12, 54));
-          assertEquals(h.getRadiusCorner(), 0);
+        assertEquals(h.getRadiusCorner(), 0);
     }
     
     @Test
     void testGetRadiusBaseZero() {
         Heptagon h = new Heptagon(0, 49, new Point(57, -82));
-          assertEquals(h.getRadiusbase(), 0);
+        assertEquals(h.getRadiusbase(), 0);
     }
     
     @Test
     void testGetRadiusCorner() {
         Heptagon h = new Heptagon(15, 21, new Point(-12, 54));
-          assertEquals(h.getRadiusCorner(), 17.3, 0.1);
+        assertEquals(h.getRadiusCorner(), 17.3, 0.1);
     }
     
     @Test
     void testGetRadiusBase() {
         Heptagon h = new Heptagon(15, 49, new Point(57, -82));
-          assertEquals(h.getRadiusbase(), 15.6, 0.1);
+        assertEquals(h.getRadiusbase(), 15.6, 0.1);
     }
 }
