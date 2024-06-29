3e7d9d7fe7599b67d1ae04adcd6e8d9f2121d949
jqlliu <2260216963liu@gmail.com>
2024-06-28 23:27:56 -0400

    Fixed bugs
    All tests now pass

--- Heptagon.java
+++ Heptagon.java
@@ -1,31 +1,32 @@
-package lab4;
+package lab5;
+
 public class Heptagon {
-	double sideLength, rotation; 
+	double sideLength, rotation;
 	Point position;
-	
+
 	Heptagon(double sideLength, double rotation, Point position){
-		this.sideLength = sideLength; 
+		this.sideLength = sideLength;
 		this.rotation = rotation;
 		this.position = position;
 		//Simplify rotation
-		this.rotation = rotation % (Math.pi*2/7);
+		this.rotation = rotation % ( Math.PI*2/7);
 	}
-	
+
 	double getArea(){
-		return (7/4)*sideLength*sideLength*(1/Math.tan(Math.pi*2/7));
+		return (7/4)*sideLength*sideLength*(1/Math.tan( Math.PI/7));
 	}
-	
-	double getPerim(){
+
+	double getPerimeter(){
 		return 7 * sideLength;
 	}
-	
+
 	double getRadiusCorner(){
-		return Math.cos((900 / 180) * Math.pi /14) * sideLength / 2;
+		return (sideLength / 2)/Math.cos((900 / 180) *  Math.PI /14);
 	}
-	
+
 	double getRadiusBase(){
-		return Math.tan((900 / 180) * Math.pi /14) * sideLength / 2;
+		return Math.tan((900 / 180) *  Math.PI /14) * sideLength / 2;
 	}
-	
-	
+
+
 }
