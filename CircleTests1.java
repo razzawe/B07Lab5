da2fe1719123dd22e52ec723c3d5f4e0f7417563
Ohama SB <mohamed.sbeinati@mail.utoronto.ca>
2024-06-30 20:03:45 -0400

    test: add JUnit tests for Circle class

---
+++ CircleTests1.java
@@ -0,0 +1,16 @@
+package lab5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CircleTests1 {
+
+    @Test
+    public void testGetArea() {
+        Point center = new Point(0, 0);
+        Circle circle = new Circle(center, 5);
+        assertEquals(Math.PI * 5 * 5, circle.getArea(), 0.0001);
+    }
+
+}
